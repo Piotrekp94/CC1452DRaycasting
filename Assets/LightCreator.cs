ddf2f7dc82d144fd7e037cdd8cfad58b18192fb7
piotr <piotrekp94@gmail.com>
2020-08-16 20:25:35 +0200

    Drawing obstacles and light source

---
+++ Assets/LightCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject light;
+    private Vector2 mousePosition;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) //Or use GetKeyDown with key defined with mouse button
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(light, mousePosition, Quaternion.Euler(0.0f, 0.0f, 0.0f));
+        }
+    }
+}
---
+++ Assets/LightDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDrawer : MonoBehaviour
+{
+    private void Start()
+    {
+        LineRenderer line = GetComponent<LineRenderer>();
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePositionOffset = mousePosition;
+        mousePositionOffset.x += 0.05f;
+        mousePositionOffset.y += 0.05f;
+        mousePosition.x -= 0.05f;
+        mousePosition.y -= 0.05f;
+        line.SetPosition(0, mousePosition);
+        line.SetPosition(1, mousePositionOffset);
+    }
+}
+
